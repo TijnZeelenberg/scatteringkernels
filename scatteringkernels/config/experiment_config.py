03ead633c2c0188361894f2e95527389c3d67872
Tijn Zeelenberg <tijnqz@gmail.com>
2026-02-24 12:05:10 +0100

    feat(config): add mdn dimension and gmm configs

--- scatteringkernels/config/experiment_config.py
+++ scatteringkernels/config/experiment_config.py
@@ -1,6 +1,11 @@
+from typing import Literal
+
+CovarianceType = Literal['full', 'tied', 'diag', 'spherical']
+
 class ExperimentConfig:
     def __init__(self):
-
+        
+        # MDN settings
         # Training parameters
         self.learning_rate = 1e-3
         self.batch_size = 64
@@ -10,7 +15,16 @@ class ExperimentConfig:
         self.random_seed = 42
 
         # Model parameters
+        self.input_dim = 3
+        self.output_dim = 2
         self.num_mixtures = 5
         self.hidden_dim = 128
+        
+        # Dataset parameters
+        self.num_samples = 10000
+
+        # GMM settings
+        self.gmm_n_components = 5
+        self.gmm_covariance_type:CovarianceType = 'full'
 
 
