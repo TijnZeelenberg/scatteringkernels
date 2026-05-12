3ffb725eb4f5ad8bfd8726a3ea978b2498022081
Tijn Zeelenberg <tijnqz@gmail.com>
2026-05-12 09:49:24 +0200

    refactor: rename files for brevity

--- training/wfsweep.py
+++ training/wfsweep.py
@@ -4,12 +4,12 @@ from tqdm import tqdm
 
 
 config = ExperimentConfig()
-datapath = "data/O2O2_collisions_uniform.npy"
+datapath = "data/H2H2_collisions_numba_b1_0_400000_seed42.npy"
 weights = [0.25, 0.5, 1, 2, 3, 4, 5, 6, 7]
 for wf in tqdm(
     weights, desc="Training models with different weighting factors", unit="weight"
 ):
-    outputpath = f"results/models/weightsensitivity/O2_400000_uniform/mdn_H2_wf{str(wf).replace('.', '_')}.pth"
+    outputpath = f"results/models/mdn/weightsensitivity/H2_400000_dataseed42/mdn_H2_wf{str(wf).replace('.', '_')}.pth"
     train_mdn(
         datapath,
         outputpath,
