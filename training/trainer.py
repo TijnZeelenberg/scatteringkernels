aff50bf24457b241f818be889af75419527a2c46
Tijn Zeelenberg <tijnqz@gmail.com>
2026-05-28 16:26:09 +0200

    fix: change experiment params

--- training/trainer.py
+++ training/trainer.py
@@ -14,15 +14,15 @@ __all__ = ["train_mdn"]
 if __name__ == "__main__":
     datapath = (
         paths.DATA_DIR
-        / "ctc/H2/impactparam/H2_collisions_b1_0_uniform_Erelmax10000_ncoll800000_seed42.npy"
+        / "ctc/H2/impactparam/H2_collisions_b1_55_uniform_Erelmax2000_ncoll1000000_seed42.npy"
     )
-    outputpath = paths.model_path("mdn", "mdn_H2_uniform_hiddim8_mix20")
+    outputpath = paths.model_path("mdn", "mdn_H2_b1_55_bs10000Erelmax2000.pth")
     train_mdn(
         datapath=str(datapath),
         outputpath=str(outputpath),
-        epochs=100,
+        epochs=200,
         batch_size=10000,
         lr=1.0e-4,
-        patience=100,
+        patience=200,
         showplots=True,
     )
